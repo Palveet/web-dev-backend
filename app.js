07480c4a02592e03ad6ac7bdb5f747d5e1e4663b
Palveet <palveetkaursaluja@gmail.com>
2021-01-11 22:39:19 +0530

    pug

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
-
+const path = require('path');
+//initialising app
 const app = express();
 
+//load view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+//home route
 app.get('/', (req, res) => {
-    res.send("hello world");
+    res.render('index');
 });
 
+//starting server
 app.listen(3000, function() {
     console.log('server started on port 3000..');
 });
